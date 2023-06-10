2a6ede8290d571adf057b1fb554e973386c4ec5c
yusefHajeb <watanalihsas@gmail.com>
2023-06-11 02:37:25 +0300

    (Data layer)build class PostRepository with local

---
+++ lib/core/error/failure.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+// all failure يقابله expintion why?
+// whole error expentio we are throw expentopn to class
+//
+abstract class Failure extends Equatable {}
+
+class OffLineFailure extends Failure {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class ServerFailure extends Failure {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+class EmptyCashExpention extends Failure {
+  List<Object?> get props => [];
+}
